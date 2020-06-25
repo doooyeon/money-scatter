97993f6f3c51e211d9e6bbc88e1396e66ea96d02
doooyeon <ehdus5958@naver.com>
2020-06-25 21:03:08 +0900

    Initial project

---
+++ src/main/java/com/pay/money/scatter/MoneyScatterApplication.java
@@ -0,0 +1,13 @@
+package com.pay.money.scatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyScatterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyScatterApplication.class, args);
+    }
+
+}
